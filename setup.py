1a0a02bc4ca55747146d986792007ee47b3c133f
Parallels <nrbontha@gmail.com>
2018-06-05 12:01:36 -0500

    Add setup.py, license, manifest, and configs

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# get long description from README
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pd_validator',  
+    version='0.1.1',  
+    description='pandas DataFrame validation library',  
+    long_description=long_description,  
+    long_description_content_type='text/markdown', 
+    url='https://github.com/nrbontha/pd-validator', 
+    author='nrbontha',  
+    author_email='nrbontha@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Data Validation',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords=['pandas', 'data', 'validation', 'analysis'],  
+    packages=find_packages(exclude=['contrib', 'docs']), 
+    install_requires=['pandas', 'numpy']
+)
+
